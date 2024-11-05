a36e5e9e4bb135b58178d30029153848f4750f62
hinkleydev <azz@hinkley.dev>
2024-11-05 11:22:09 +0000

    Done code for part 3

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
 
-const port = 3000;
+// Body processing middleware
+app.use(express.urlencoded());
+app.use(express.json());
+
+// --- CREATE operations ---
+
+// Add new musician
+app.post("/musicians", async function(req, res) {
+    const name = req.body.name;
+    const instrument = req.body.instrument;
+    const bandId = req.body.bandId;
+    await Musician.create({name, instrument, bandId}) // Shorthand object syntax
+    const allMusicians = await Musician.findAll();
+    res.json(allMusicians);
+})
+
+// --- READ operations ---
 
 // Return all musicians 
 app.get("/musicians", async function(req, res) {
@@ -17,5 +32,28 @@ app.get("/musicians/:id", async function(req, res) {
     res.json(musician);
 })
 
+// --- UPDATE operations ---
+
+// Update specific musician
+app.put("/musicians/:id", async function(req, res) {
+    const musician = await Musician.findByPk(req.params.id);
+    const name = req.body.name;
+    const instrument = req.body.instrument;
+    const bandId = req.body.bandId;
+    await musician.update({name, instrument, bandId}) // Shorthand object
+    const allMusicians = await Musician.findAll();
+    res.json(allMusicians);
+})
+
+// --- DELETE operations ---
+
+// Delete specific musician
+app.delete("/musicians/:id", async function(req, res) {
+    const musician = await Musician.findByPk(req.params.id);
+    await musician.destroy();
+    res.json({"message" : "Deleted"})
+})
+
+// -- END operations ---
 
 module.exports = app;
