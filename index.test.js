af0e55c8a0c8eefd6031b45c082f51bb590a27b3
hinkleydev <azz@hinkley.dev>
2024-11-07 16:07:11 +0000

    Clean up of test file

--- index.test.js
+++ index.test.js
@@ -1,13 +1,9 @@
-// install dependencies
 const { execSync } = require('child_process');
-//execSync('npm install'); // Don't really need this
-execSync('npm run seed');
+execSync('npm run seed'); // Refill data for a clean test
 
 const request = require("supertest")
-const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
-const {seedMusician} = require("./seedData");
 const { expect, it, describe } = require("@jest/globals");
 
 // --- CREATE operations ---
